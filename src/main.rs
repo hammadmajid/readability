1fbbc4083a71a8c390034adfce0e0f2686147a70
hammadmajid <hm.hammadmajid@gmail.com>
2023-02-08 23:48:20 +0500

    Check argument vector and exit with appopriate message and code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    if std::env::args().len() != 3 {
+        logger(
+            "Test readability score of your content\n".to_owned()
+                + "\nUsage: readability \"Text string\" [Method]\n"
+                + "\nText string:\n\tThe string of text you want to test.\n"
+                + "\nMethod:\n\tThe test method you want to use, possible values are:\n"
+                + "\t\tDale-Chall Score\n\t\tColeman-Liau index",
+        );
+        std::process::exit(1);
+    }
+}
+
+fn logger(message: String) {
+    println!("{message}");
 }
