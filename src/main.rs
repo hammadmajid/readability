ff8d1ea690391e7e61b124a42cbc6c3cd8864ec8
hammadmajid <hm.hammadmajid@gmail.com>
2023-03-06 17:40:44 +0000

    Refactor: write fn for exiting out with guide
    Remove the logger function as it was not necessary
    
    Update the usage guide
    
    Extract the text from argv

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
+use std::env;
+use std::process;
+
 fn main() {
-    if std::env::args().len() != 3 {
-        logger(
-            "Test readability score of your content\n".to_owned()
-                + "\nUsage: readability \"Text string\" [Method]\n"
-                + "\nText string:\n\tThe string of text you want to test.\n"
-                + "\nMethod:\n\tThe test method you want to use, possible values are:\n"
-                + "\t\tDale-Chall Score\n\t\tColeman-Liau index",
-        );
-        std::process::exit(1);
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        exit_with_usage_guide();
     }
+
+    let _text = args[2].clone();
+    // let method = // TODO(hammadmajid): extract the [1] argument
 }
 
-fn logger(message: String) {
-    println!("{message}");
+fn exit_with_usage_guide() {
+    println!("\
+    Usage: cargo run [method] [text]
+
+        [method]    The test method you want to use, possible values are:
+                        DaleChallScore
+                        ColemanLiauIndex
+
+        [text]      The string of text you want to test.
+    ");
+    process::exit(1);
 }
