3574dab6f9b07d4e3c8103b74ad0f7bc5b5f71ad
hammadmajid <hm.hammadmajid@gmail.com>
2023-03-07 12:17:59 +0000

    Feat: extract method from argv

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::process;
+use std::str::FromStr;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,8 +9,36 @@ fn main() {
         exit_with_usage_guide();
     }
 
-    let _text = args[2].clone();
-    // let method = // TODO(hammadmajid): extract the [1] argument
+    let text = args[2].clone();
+
+    #[allow(non_snake_case)]
+    let method = match Method::from_str(&args[1]) {
+        Ok(Method) => Method,
+        Err(_err) => {
+            exit_with_usage_guide();
+            return;
+        }
+    };
+
+    println!("text: {}\nmethod: {:?}", text, method);
+}
+
+#[derive(Debug)]
+enum Method {
+    DaleChallScore,
+    ColemanLiauIndex,
+}
+
+impl FromStr for Method {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "DaleChallScore" => Ok(Method::DaleChallScore),
+            "ColemanLiauIndex" => Ok(Method::ColemanLiauIndex),
+            _ => Err(())
+        }
+    }
 }
 
 fn exit_with_usage_guide() {
