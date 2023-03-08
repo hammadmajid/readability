4d1c8e5235c0359e4a89ad5230ebc1a7af531a79
hammadmajid <hm.hammadmajid@gmail.com>
2023-03-08 14:50:13 +0000

    feat(calc): write skelton function for each test method

---
+++ src/calc.rs
@@ -0,0 +1,31 @@
+pub fn dale_chall_score(text: String) {
+
+}
+
+pub fn coleman_liau_index(text: String) {
+
+}
+
+pub fn automated_readability_index(text: String) {
+
+}
+
+pub fn flesch_kincaid_grade_level(text: String) {
+
+}
+
+pub fn flesch_kincaid_reading_ease(text: String) {
+
+}
+
+pub fn gunning_fog(text: String) {
+
+}
+
+pub fn lix(text: String) {
+
+}
+
+pub fn smog(text: String) {
+
+}
