5fa841657e0c8ea0be0a0f6c2668dfa6b53c43fe
hammadmajid <hm.hammadmajid@gmail.com>
2023-03-08 14:50:47 +0000

    feat(calc): call the respective function for each method

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+mod calc;
+
 use clap::{Parser, ValueEnum};
 
 fn main() {
     let cli = Cli::parse();
 
-    println!("{:?}", cli);
+    match cli.method {
+        Method::DaleChallScore => calc::dale_chall_score(cli.text),
+        Method::ColemanLiauIndex => calc::coleman_liau_index(cli.text),
+        Method::AutomatedReadabilityIndex => calc::automated_readability_index(cli.text),
+        Method::FleschKincaidGradeLevel => calc::flesch_kincaid_grade_level(cli.text),
+        Method::FleschKincaidReadingEase => calc::flesch_kincaid_reading_ease(cli.text),
+        Method::GunningFog => calc::gunning_fog(cli.text),
+        Method::Lix => calc::lix(cli.text),
+        Method::Smog => calc::smog(cli.text),
+    }
 }
 
 #[derive(Parser, Debug)]
