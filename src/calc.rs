51ad4fde647c6705dfee3081f84e91af73f370a9
hammadmajid <hm.hammadmajid@gmail.com>
2023-03-11 09:20:54 +0000

    Add: todo! macro for unfinished code

--- src/calc.rs
+++ src/calc.rs
@@ -1,31 +1,31 @@
 pub fn dale_chall_score(_text: String) -> i8 {
-    0
+    todo!()
 }
 
 pub fn coleman_liau_index(_text: String) -> i8 {
-    0
+    todo!()
 }
 
 pub fn automated_readability_index(_text: String) -> i8 {
-    0
+    todo!()
 }
 
 pub fn flesch_kincaid_grade_level(_text: String) -> i8 {
-    0
+    todo!()
 }
 
 pub fn flesch_kincaid_reading_ease(_text: String) -> i8 {
-    0
+    todo!()
 }
 
 pub fn gunning_fog(_text: String) -> i8 {
-    0
+    todo!()
 }
 
 pub fn lix(_text: String) -> i8 {
-    0
+    todo!()
 }
 
 pub fn smog(_text: String) -> i8 {
-    0
+    todo!()
 }
